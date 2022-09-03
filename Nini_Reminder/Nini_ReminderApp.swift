c07c317f84578e535f5dd35e580e6ff2cae5465a
PopekD <dawid.popek01@gmail.com>
2022-09-03 23:48:42 +0300

    Initial Commit

---
+++ Nini_Reminder/Nini_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nini_ReminderApp.swift
+//  Nini_Reminder
+//
+//  Created by Nini on 3.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct Nini_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
